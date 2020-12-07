171155d5fec280c11432b40b12c30604af75a1a5
athiruppathi <arjunthiruppathi@gmail.com>
2020-12-07 11:54:21 -0500

    deleted

--- fund_insights/spiders/blackrock.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class BlackrockSpider(scrapy.Spider):
-    name = 'blackrock'
-    start_urls = ['https://www.blackrock.com/us/individual/insights']
-
-    def parse(self, response):
-        pass
--- fund_insights/spiders/bridgewater.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class BridgewaterSpider(scrapy.Spider):
-    name = 'bridgewater'
-    start_urls = ['https://www.bridgewater.com/research-and-insights']
-
-    def parse(self, response):
-        pass
